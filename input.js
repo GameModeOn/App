7914d8b98c17d6c4c94a4b6e7dc654b6769bd3e1
lub <github@lubiland.de>
2014-08-06 20:09:32 +0200

    debugging

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 function input(msg) {
+	alert(msg);
 	$('#input').val('..loading..');
 	$.post(api+'input.php', { sid: sid, msg: msg }).done(function(data) {
+		alert(data);
 		if(data == 'true') {
 			$('#msg').val('');
 			$('#input').val('SHOUT');
