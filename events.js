a04e82889e486b74273b52f9d2837585a7463515
Simon Körner <github@lubiland.de>
2014-08-06 14:29:54 +0200

    typo

--- events.js
+++ events.js
@@ -4,5 +4,6 @@ document.addEventListener('deviceready', init, true);
 $('[name="user"]').click(cleardefaultcontent);
 $('[name="passwd"]').click(cleardefaultcontent);
 
+
 //login.js
 $('#btnLogin').click(login);
