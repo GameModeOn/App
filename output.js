a0dee525bb8df106659affddcaefad7acebea1a7
Simon Körner <github@lubiland.de>
2014-08-08 14:26:35 +0200

    fix output

--- output.js
+++ output.js
@@ -1,7 +1,7 @@
 function output() {
 	$.post(api+'output.php', { sid: sid }).done(function(data) {
-		alert(data);
 		$('#output').text(data);
 	});
-	setTimeout("output()", 3000);
+	setTimeout('output()', 3000);
+	$('#output').show();
 }
