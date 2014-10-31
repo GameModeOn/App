02b5466fe23b9f9fd6fb947f49fdb10b6a4eb34c
lub <github@lubiland.de>
2014-10-31 19:29:00 +0100

    remove push

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ errmsg = 'Error! Retry?';
 
 
 function init() {
-	pushNotification = window.plugins.pushNotification;
 	if(window.localStorage.getItem('autologin') == 'true') {
 		var user = window.localStorage.getItem('user');
 		var passwd = window.localStorage.getItem('passwd');
