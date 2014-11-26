cd176c17696e3d6a3c8f85bbb5c9d045fec755a7
lub <github@lubiland.de>
2014-11-26 23:33:03 +0100

    debug uploadimage

--- imageshare.js
+++ imageshare.js
@@ -13,6 +13,7 @@ function pickimage() {
 function uploadimage(fileURL) {
 	spinnerplugin.show();
 	var ft = new FileTransfer();
+	ft.onprogress = function(progress) { alert(progress.loaded+' / '+progress.total); };
 	ft.upload(fileURL, encodeURI('https://mediacru.sh/api/upload/file'), function(success) { alert(success); }, function(error) { alert(error); });
 	spinnerplugin.hide();
 }
