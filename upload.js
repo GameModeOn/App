6cf89e2bc039ee9e18204f139230e3bc9518a996
lub <github@lubiland.de>
2014-11-27 18:48:18 +0100

    debug upload

--- upload.js
+++ upload.js
@@ -1,12 +1,12 @@
 function upload() {
 	var file = document.getElementById('fileToUpload').files[0];
 	alert(file);
-	spinnerplugin.show();
+/*	spinnerplugin.show();
 	MediaCrush.upload(file, function(media) {
 		media.wait(function() {
 			spinnerplugin.hide();
 			alert(media.status);
 			alert(media.url);
 		});
-	});
+	});*/
 }
