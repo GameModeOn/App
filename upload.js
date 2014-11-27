7b4f6dc973ad0351ed0950acb0aed3132b2a8c26
lub <github@lubiland.de>
2014-11-27 20:14:05 +0100

    debug upload

--- upload.js
+++ upload.js
@@ -1,3 +1,36 @@
+function ObjectDump(obj, name) {
+  this.result = "[ " + name + " ]\n";
+    this.indent = 0;
+     
+       this.dumpLayer = function(obj) {
+           this.indent += 2;
+	    
+	        for (var i in obj) {
+		      if(typeof(obj[i]) == "object") {
+		              this.result += "\n" +
+			                "              ".substring(0,this.indent) + i +
+					          ": " + "\n";
+						          this.dumpLayer(obj[i]);
+							        } else {
+								        this.result +=
+									          "              ".substring(0,this.indent) + i +
+										            ": " + obj[i] + "\n";
+											          }
+												      }
+												       
+												           this.indent -= 2;
+													     }
+													      
+													        this.showResult = function() {
+														    var pre = document.createElement('pre');
+														        pre.innerHTML = this.result;
+															    document.body.appendChild(pre);
+															      }
+															       
+															         this.dumpLayer(obj);
+																   this.showResult();
+																   }
+
 function selectupload() {
 	navigator.camera.getPicture(upload,
 		function(e) {
@@ -11,7 +44,7 @@ function selectupload() {
 function upload(file) {
 	var ft = new FileTransfer();
 	ft.onprogress = function(progress) { alert(progress.loaded+' / '+progress.total); };
-	ft.upload(file, encodeURI('https://mediacru.sh/api/upload/file'), function(success) { alert(success.hash); }, function(error) { alert(error); });
+	ft.upload(file, encodeURI('https://mediacru.sh/api/upload/file'), function(success) { alert(ObjectDump(success, 'success')); }, function(error) { alert(ObjectDump(error, 'error')); });
 	
 /*	spinnerplugin.show();
 	MediaCrush.upload(file, function(media) {
