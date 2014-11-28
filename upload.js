923793fbb97c459260ffff74167a36b22da741af
lub <github@lubiland.de>
2014-11-28 19:14:18 +0100

    input uploaded image

--- upload.js
+++ upload.js
@@ -19,6 +19,6 @@ function upload(file) {
 	var ft = new FileTransfer();
 
 	spinnerplugin.show;
-	ft.upload(file, uri, function(success) { alert('success: '+success.response); }, function() { alert('ERROR!'); }, options);
+	ft.upload(file, uri, function(r) { input('https://gamemodeon.de/app/uploads/'+r.response); }, function() { alert('ERROR!'); }, options);
 	spinnerplugin.hide;
 }
