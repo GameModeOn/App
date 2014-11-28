a2d0baca45a30c2366cffdf46536d5646fd54960
lub <github@lubiland.de>
2014-11-28 18:38:39 +0100

    del mediacrush

--- upload.js
+++ upload.js
@@ -13,14 +13,4 @@ function upload(file) {
 	var ft = new FileTransfer();
 	ft.onprogress = function(progress) { alert(progress.loaded+' / '+progress.total); };
 	ft.upload(file, encodeURI(api+'upload.php'), function(success) { alert('success: '+success.hash); }, function(error) { alert('error: '+erro); });
-/*	ft.upload(file, encodeURI('https://mediacru.sh/api/upload/file'), function(success) { alert('success: '+success.hash); }, function(error) { alert('error: '+erro); });
-	
-/*	spinnerplugin.show();
-	MediaCrush.upload(file, function(media) {
-		media.wait(function() {
-			spinnerplugin.hide();
-			alert(media.status);
-			alert(media.url);
-		});
-	});*/
 }
