f73b8eb57e06d9a88654b634346381c1391bb537
lub <github@lubiland.de>
2014-12-17 00:25:39 +0100

    output md5

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ function login(user, passwd) {
 	$.post(api+'login.php', { user: user, passwd: passwd }).done(function(data) {
 		if(data != '') {
 			sid = data;
+			$('#output').html(window.localStorage.getItem('lastOutput');
 			output();
 			
 			$('#login').hide();
--- output.js
+++ output.js
@@ -1,6 +1,13 @@
 function output() {
-	$.post(api+'output.php', { sid: sid }).done(function(data) {
-		$('#output').html(data);
+	$.post(api+'output.php', { sid: sid, md5ob: window.localStorage.getItem('lastOutputMD5') }).done(function(data) {
+		var regex = new RegExp('<!--(.*)-->');
+		var regex = regex.exec(data);
+		if(windows.localStorage.getItem('lastOutputMD5') != regex[1]) {
+			window.localStorage.setItem('lastOutputMD5', regex[1]);
+			window.localStorage.setItem('lastOutput', data);
+			$('#output').html(data);
+			window.plugins.toast.showLongBottom('new content');
+		}
 	});
 	setTimeout('output()', 3000);
 	$('#output').show();
