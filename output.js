7f4446f01713a881d0e0c4321c5b87889e35d6ad
lub <github@lubiland.de>
2014-12-17 00:54:27 +0100

    rm output update spinner

--- output.js
+++ output.js
@@ -3,11 +3,9 @@ function output() {
 		var regex = new RegExp('<!--(.*?)-->');
 		var regex = regex.exec(data);
 		if(window.localStorage.getItem('lastOutputMD5') != regex[1]) {
-			spinnerplugin.show();
 			window.localStorage.setItem('lastOutputMD5', regex[1]);
 			window.localStorage.setItem('lastOutput', data);
 			$('#output').html(data);
-			spinnerplugin.hide();
 		}
 	});
 	setTimeout('output()', 3000);
