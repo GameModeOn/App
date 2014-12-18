bfcfcae8c7ad18c293e47c99ecead32db6436702
lub <github@lubiland.de>
2014-12-18 14:39:18 +0100

    asef

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ errmsg = 'Error! Retry?';
 
 
 function init() {
+	window.localStorage.removeItem('lastOutput');
+	
 	if(window.localStorage.getItem('autologin') == 'true') {
 		var user = window.localStorage.getItem('user');
 		var passwd = window.localStorage.getItem('passwd');
