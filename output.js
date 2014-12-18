6dd4ed0a60a33a92710b8cfdadd510e34941e06c
lub <github@lubiland.de>
2014-12-18 14:43:31 +0100

    remove debug

--- output.js
+++ output.js
@@ -1,15 +1,12 @@
 function output() {
-	alert(window.localStorage.getItem('lastOutput'));
 	$.post(api+'output.php', { sid: sid, md5ob: window.localStorage.getItem('lastOutput') }).done(function(data) {
-		alert(data);
 		var regex = new RegExp('<!--(.*?)-->');
 		var regex = regex.exec(data);
 		if(window.localStorage.getItem('lastOutput') != regex[1]) {
-			alert('quark');
 			window.localStorage.setItem('lastOutput', regex[1]);
 			$('#output').html(data);
 		}
 	});
-	setTimeout('output()', 3000);
+	setTimeout('output()', 1000);
 	$('#output').show();
 }
