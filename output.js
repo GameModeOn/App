dea476c0e8db583acace06255fdacac8c276631b
lub <github@lubiland.de>
2014-12-19 17:21:24 +0100

    fix md5

--- output.js
+++ output.js
@@ -1,7 +1,5 @@
 function output(md5) {
-	alert('begin '+md5);
 	$.post(api+'output.php', { sid: sid, md5ob: md5 }).done(function(data) {
-		alert('post '+md5);
 		var regex = new RegExp('<!--(.*?)-->');
 		var regex = regex.exec(data);
 		if(md5 != regex[1]) {
