1935290d2e17762de936a4e02679eff8ad628bb2
lub <github@lubiland.de>
2014-12-23 21:46:18 +0100

    finish eco mode

--- menu.js
+++ menu.js
@@ -14,9 +14,9 @@ function initMenu() {
 		window.localStorage.setItem('eco', 'false');
 	}
 	if(window.localStorage.getItem('eco') == 'true') {
-//		$('#eco').prop('checked') = 'true';
+		$('#eco').prop('checked', 'true');
 	} else {
-//		$('#eco').prop('checked') = 'false';
+		$('#eco').prop('checked', 'false');
 	}
 }
 
