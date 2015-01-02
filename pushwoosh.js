432c9fc038501186d3538252e664e79d2a7675fa
lub <github@lubiland.de>
2015-01-02 12:30:04 +0100

    tidy

--- pushwoosh.js
+++ pushwoosh.js
@@ -1,18 +1,6 @@
 function initPushwoosh() {
 	var pushNotification = window.plugins.pushNotification;
 	
-/*	//set push notifications handler
-	document.addEventListener('push-notification', function(event) {
-		var title = event.notification.title;
-		var userData = event.notification.userdata;
-		
-		if(typeof(userData) != undefined) {
-			console.warn('user data: ' + JSON.stringify(userData));
-		}
-		
-		alert(title);
-	});*/
-	
 	//initialize Pushwoosh with projectid: GOOGLE_PROJECT_ID, appid : PUSHWOOSH_APP_ID. This will trigger all pending push notifications on start.
 	pushNotification.onDeviceReady({ projectid: '17831485163', appid : '1D863-3883F' });
 	
@@ -20,10 +8,8 @@ function initPushwoosh() {
 	pushNotification.registerDevice(
 		function(status) {
 			var pushToken = status;
-			console.warn('push token: ' + pushToken);
 		},
 		function(status) {
-			console.warn(JSON.stringify(['failed to register ', status]));
 		}
 	);
 }
