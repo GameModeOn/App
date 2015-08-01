5b8b03f9f3557a4e04a9165affe19fc2f3120f69
Simon Körner <github@lubiland.de>
2015-08-01 18:21:36 +0200

    switch from a comment to <md5obHASHmd5ob>

--- output.js
+++ output.js
@@ -1,6 +1,6 @@
 function output(md5) {
 	$.post(api+'output.php', { sid: sid, md5ob: md5, eco: window.localStorage.getItem('eco') }).done(function(data) {
-		var regex = new RegExp('<!--(.*?)-->');
+		var regex = new RegExp('<md5ob(.*?)md5ob>');
 		var regex = regex.exec(data);
 		if(md5 != regex[1]) {
 			md5 = regex[1];
